c546d4fed84109ca7806d923b26f8d2c515fa695
Rossenzii <jangmj80@naver.com>
2025-06-25 15:31:23 +0900

    [Fix] vpc name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 module "backend_vpc" {
   source     = "./modules/backend"
   cidr_block = "10.0.0.0/16"
-  vpc_name   = "-backend"
+  vpc_name   = "rossenzii-backend"
 }
 ## db
 module "dynamodb_lock_table" {
